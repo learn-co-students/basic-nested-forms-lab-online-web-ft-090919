966165fc881a144eedaa71e897f2f93e7ae137dc
averagealloy <m.mctighe10@gmail.com>
2019-11-20 10:19:54 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,20 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = Recipe.new #set var recipe to recipe.new 
+    @recipe.ingredients.build(name: "First Ingredient") #a recipe will have a name space for the first ingredient when made 
+    @recipe.ingredients.build(name: "Second Ingredient") #a recipe will have a name space for the first ingredient when made
   end
 
   def create
+    Recipe.create(recipe_params) #create a new recipe with the params hash 
+    redirect_to recipes_path #sends to recipe path 
   end
+
+  private #I dont want the user to be able to change this 
+
+  def recipe_params #this is what i dont waht the user to change 
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+  end 
 end
+
